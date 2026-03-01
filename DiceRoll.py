cd6a2c36781a8d8a733aab7787eda84d4e525e01
growgavin4-sudo <growgavin4@gmail.com>
2026-03-01 12:07:00 -0600

    Add dice rolling functionality and author details
    Updated author information and added dice rolling logic.

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,18 +1,23 @@
 #DiceRoll.py
-#Name:
-#Date:
-#Assignment:
+#Name:Gavin Grow
+#Date:03/1/26
+#Assignment:dice roll
 import random
 
 def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
+  for r in range(100):
+    dice1 = random.randint(1,6)
+    rolls[dice1 - 1] = rolls[dice1 - 1] + 1
   #find the sum total of the two dice
   
   #print statictics for dice rolls
-
+dice = 1
+for count in rolls: 
+  print(dice, ":", count)
+  dice = dice + 1
 
 if __name__ == '__main__':
   main()
